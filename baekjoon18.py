ffed625158494181c0d80ca5feec1b90b44d0154
headacheduck <khwk04684@naver.com>
2024-09-30 16:08:07 +0900

    second commit

--- baekjoon18.py
+++ baekjoon18.py
@@ -3,10 +3,13 @@ H, M = map(int,input().split())
 if M >= 45 :
     after_M = M - 45
     print(H, after_M)
+
 if M < 45 :
     after_H = H - 1 
     after_M = 60+(M-45)
+
     if after_H == -1 :
         print(int(after_H + 24), after_M)
+        
     else :
         print(after_H, after_M)
